42bf497bca9c92d2ba685877e5fc15a7034060cc
Thomas Banks <thomas.banks@havaslynx.com>
2019-04-10 12:13:55 +0100

    moved logging to screen for mobile testing

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import { helloVariable } from './@functions';
 import { addTwoNumbers, multiplyTwoNumbers } from './@helpers';
+const main = document.querySelector('main')
 
 let arr = [1,2,3,4,5,6,7,8,9]
 
@@ -7,3 +8,10 @@ arr = arr.map(a => multiplyTwoNumbers(a,2))
 
 const result = addTwoNumbers(10,98)
 console.log(helloVariable('thomas'), result, arr);
+
+main.innerHTML += `<h1>${helloVariable('thomas')}</h1>`
+main.innerHTML += `10 + 98 = ${result}`
+main.innerHTML += `<ul></ul>`
+arr.map(a => {
+  main.querySelector('ul').innerHTML += `<li>${a}</li>`
+})
